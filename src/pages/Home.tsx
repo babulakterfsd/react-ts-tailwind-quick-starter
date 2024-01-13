1c2b7c4324a0f6dae8438ee34633866d91defa36
Babul Akter <babulakterfsd@gmail.com>
2024-01-13 14:55:05 +0600

    about page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ import CreatorInfo from '../components/Creator';
 const Home = () => {
   return (
     <div className="h-screen flex justify-center items-center flex-col">
-      <h1 className="text-red-700 text-3xl">Welcome to React Quick Starter</h1>
+      <h1 className="text-red-700 text-3xl text-center">
+        Welcome to React Quick Starter
+      </h1>
       <CreatorInfo />
     </div>
   );
