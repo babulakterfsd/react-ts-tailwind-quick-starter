bae00a8553d854df118fe741652ec60f2e4651be
Babul Akter <babulakterfsd@gmail.com>
2024-01-13 23:10:14 +0600

    todo page added

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,9 +1,13 @@
 const Todo = () => {
   return (
     <div>
-      <h3 className="text-xl font-semibold text-center">
+      <h3 className="text-3xl text-center text-red-700">
         Let's create a todo app with redux
       </h3>
+      <p className="text-center text-sm max-w-96 mx-auto mt-2">
+        This is a simple todo app that helps to understand the basics of redux
+        and how to setup the boilerplate for redux.
+      </p>
     </div>
   );
 };
