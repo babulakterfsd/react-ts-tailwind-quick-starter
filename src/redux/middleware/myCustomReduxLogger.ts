1dff5b82ca3361bd8cf248ee4181ef106a3a640d
Babul Akter <babulakterfsd@gmail.com>
2024-01-14 00:49:41 +0600

    redux setup done with typescript

--- src/redux/middleware/myCustomReduxLogger.ts
+++ src/redux/middleware/myCustomReduxLogger.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const myCustomReduxLogger =
   (store: any) => (next: any) => (action: any) => {
+    console.log(store);
     next(action);
   };
