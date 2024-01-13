6cef176b497335b8df755018213af22f3321c710
Babul Akter <babulakterfsd@gmail.com>
2024-01-13 14:34:48 +0600

    about page added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
       <h1 className="text-red-700 text-3xl">I am Babul Akter</h1>
       <p>
         Developing responsive modern UI is my strength. Can create scalable
-        backend with node, express, mongoose & mongodb. Expert in full-stack
+        backend with node, express, mongoose and mongodb. Expert in full-stack
         solution with nextjs.
       </p>
     </div>
