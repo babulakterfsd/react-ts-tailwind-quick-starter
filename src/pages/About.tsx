307a04d3fb80ff2c2804a609fd9c8bcd14e10700
Babul Akter <babulakterfsd@gmail.com>
2024-01-13 14:22:32 +0600

    project initialized

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      <h1 className="text-red-700 text-3xl">I am Babul Akter</h1>
+      <p>
+        Developing responsive modern UI is my strength. Can create scalable
+        backend with node, express, mongoose & mongodb. Expert in full-stack
+        solution with nextjs.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import CreatorInfo from '../components/Creator';
+
+const Home = () => {
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      <h1 className="text-red-700 text-3xl">Welcome to React Quick Starter</h1>
+      <CreatorInfo />
+    </div>
+  );
+};
+
+export default Home;
