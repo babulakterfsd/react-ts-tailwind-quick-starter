2ac2cf9006cb0a25aacb9c69e9298184c170d0e7
Babul Akter <babulakterfsd@gmail.com>
2024-01-15 23:45:03 +0600

    todo filtering implemented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,17 @@ const Home = () => {
         Welcome to React Quick Starter
       </h1>
       <CreatorInfo />
+      <h4 className="text-center mt-8">
+        Here you will get a completed project setup with the latest version of:
+      </h4>
+      <ul className="font-semibold text-sm mx-auto">
+        <li>React</li>
+        <li>Typescript</li>
+        <li>Tailwind CSS</li>
+        <li>Tanstack Router</li>
+        <li>Shadcn</li>
+        <li>Redux</li>
+      </ul>
     </>
   );
 };
